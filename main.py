54dd95eafd4810c6e7e5eeabd3955d6da233dcd1
beinghumbleguy <91289834+beinghumbleguy@users.noreply.github.com>
2025-03-18 12:19:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1078,7 +1078,7 @@ async def convert_link_to_button(message: types.Message):
         logger.info(f"Adding buttons because 'reflink' found in message: {message.text}")
         keyboard = InlineKeyboardMarkup(inline_keyboard=[
             # Add "Join VIP" button if the channel is in VIP_CHANNEL_IDS
-            [InlineKeyboardButton(text="🌟🚀 Join VIP", url="https://t.me/HumbleMoonshotsPay_bot")] 
+            [InlineKeyboardButton(text="🌟🚀 Join VIP 🚀🌟", url="https://t.me/HumbleMoonshotsPay_bot?start")] 
             if message.chat.id in VIP_CHANNEL_IDS else [],
             [
                 InlineKeyboardButton(text="Bloom", url=f"https://t.me/BloomSolana_bot?start=ref_humbleguy_ca_{ca}"),
